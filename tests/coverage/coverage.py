1af66d1ee4ab6315715f13d3e9d5fe686d55e201
Chandra Sanapala <chandra@sundeck.io>
2024-08-20 08:02:35 +0530

    fix python style-check errors

--- tests/coverage/coverage.py
+++ tests/coverage/coverage.py
@@ -11,12 +11,11 @@ def parse_one_file(file_path):
     parse_stream(FileStream(file_path))
 
 
-def parse_string(input_string, rule_name='doc'):
+def parse_string(input_string):
     return parse_stream(InputStream(input_string))
 
 
 def parse_stream(input_stream):
-
     # Create a lexer and parser
     lexer = TestFileLexer(input_stream)
     token_stream = CommonTokenStream(lexer)
@@ -38,7 +37,8 @@ def parse_stream(input_stream):
 
 
 def parse_basic_example():
-    parse_string('''### SUBSTRAIT_SCALAR_TEST: v1.0
+    parse_string(
+        """### SUBSTRAIT_SCALAR_TEST: v1.0
 ### SUBSTRAIT_INCLUDE: '/extensions/functions_arithmetic.yaml'
 
 # basic: 'Basic examples without any special cases'
@@ -47,29 +47,34 @@ add(100::i16, 100::i16) = 200::i16
 
 # overflow: Examples demonstrating overflow behavior
 add(120::i8, 10::i8) [overflow:ERROR] = <!ERROR>
-''')
+"""
+    )
 
 
 def parse_date_time_example():
-    parse_string('''### SUBSTRAIT_SCALAR_TEST: v1.0
+    parse_string(
+        """### SUBSTRAIT_SCALAR_TEST: v1.0
     ### SUBSTRAIT_INCLUDE: '/extensions/functions_datetime.yaml'
 
 # timestamps: examples using the timestamp type
 lt('2016-12-31T13:30:15'::ts, '2017-12-31T13:30:15'::ts) = true::bool
-''')
+"""
+    )
 
 
 def parse_decimal_example():
-    parse_string('''### SUBSTRAIT_SCALAR_TEST: v1.0
+    parse_string(
+        """### SUBSTRAIT_SCALAR_TEST: v1.0
 ### SUBSTRAIT_INCLUDE: 'extensions/functions_arithmetic_decimal.yaml'
 
 # basic: Basic examples without any special cases
 power(8::dec, 2::dec<38, 0>) = 64::fp64
 power(1.0::dec, -1.0::dec<38, 0>) = 1.0::fp64
-''')
+"""
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # TODO move these tests to test folder
     parse_basic_example()
     parse_date_time_example()
--- tests/coverage/visitor.py
+++ tests/coverage/visitor.py
@@ -4,15 +4,16 @@ from antlr4 import *
 from antlr_generated.TestFileParser import TestFileParser
 from antlr_generated.TestFileParserVisitor import TestFileParserVisitor
 
+
 class TestCaseVisitor(TestFileParserVisitor):
-    def visitDoc(self, ctx:TestFileParser.DocContext):
+    def visitDoc(self, ctx: TestFileParser.DocContext):
         # TODO Implement this method
         return int(ctx.getText())
 
-    def visitTestGroup(self, ctx:TestFileParser.TestGroupContext):
+    def visitTestGroup(self, ctx: TestFileParser.TestGroupContext):
         # TODO Implement this method
         return self.visitChildren(ctx)
 
-    def visitTestCase(self, ctx:TestFileParser.TestCaseContext):
+    def visitTestCase(self, ctx: TestFileParser.TestCaseContext):
         # TODO Implement this method
         return self.visitChildren(ctx)
